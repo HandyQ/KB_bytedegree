a0830ac5c80c0c7538abe884071ba82c42212a33
Q_jin <77479618+HandyQ@users.noreply.github.com>
2024-07-07 01:11:42 +0900

    Update ShippingService.java

--- src/main/java/com/kb/shop/service/ShippingService.java
+++ src/main/java/com/kb/shop/service/ShippingService.java
@@ -1,18 +1,41 @@
 package com.kb.shop.service;
 
+import lombok.AllArgsConstructor;
+import org.example.model.ShippingEntity;
+import org.example.model.ShippingRequest;
+import org.example.repository.ShippingRepository;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.List;
 
 @Service
 public class ShippingService {
   
   private final ShippingRepository ShippingRepository;
 
-  public ShippingEntity add(ShippingRequest request) {
-      return null;
+  public ShippingInfo add(ShippingRequest request) {
+      ShippingInfo shippingInfo = new ShippingInfo();
+      shippingInfo.setOrder(request.getOrder());
+      shippingInfo.setCompleted(request.getCompleted());
+      return this.shippingRepository.save(shippingInfo);
   }
   public ShippingEntity searchById(Long id) {
-      return null;
+       return this.shippingRepository.findById(id)
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
   }
   public ShippingEntity updateById(Long id, ShippingRequest request) {
-      return null;
+      ShippingInfo shippingInfo = this.searchById(id);
+      if (request.getTitle() != null) {
+          shippingEntity.setTitle(request.getTitle());
+      }
+      if (request.getOrder() != null) {
+          shippingEntity.setOrder(request.getOrder());
+      }
+      if (request.getCompleted() != null) {
+          shippingEntity.setCompleted(request.getCompleted());
+      }
+      return this.shippingRepository.save(shippingEntity);
   }
 }
