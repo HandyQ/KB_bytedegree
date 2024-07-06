fecfc06991139717867d82db5e2686d7817a2d63
Q_jin <77479618+HandyQ@users.noreply.github.com>
2024-07-07 00:51:34 +0900

    Create ShippingRepository.java

---
+++ src/main/java/com/kb/shop/repository/ShippingRepository.java
@@ -0,0 +1,7 @@
+// ShippingRepository.java
+package com.kb.shop.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ShippingRepository extends JpaRepository<ShippingInfo, Long> {
+}
