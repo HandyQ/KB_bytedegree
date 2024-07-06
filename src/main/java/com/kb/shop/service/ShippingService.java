0e7623d9d2de0ca76a8ff1d92071ccbad8ee2aa6
Q_jin <77479618+HandyQ@users.noreply.github.com>
2024-07-07 00:48:02 +0900

    Create ShippingService.java

---
+++ src/main/java/com/kb/shop/service/ShippingService.java
@@ -0,0 +1,18 @@
+package com.kb.shop.service;
+
+
+@Service
+public class ShippingService {
+  
+  private final ShippingRepository ShippingRepository;
+
+  public ShippingEntity add(ShippingRequest request) {
+      return null;
+  }
+  public ShippingEntity searchById(Long id) {
+      return null;
+  }
+  public ShippingEntity updateById(Long id, ShippingRequest request) {
+      return null;
+  }
+}
